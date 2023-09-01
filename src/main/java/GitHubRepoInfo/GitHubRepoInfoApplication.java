295d27ceba0b4b63beb47b7366079bd1d4d50f3a
Klaudia Kosta <Klaudia.Kosta@fdmgroup.com>
2023-09-01 12:20:49 +0200

    first commit

---
+++ src/main/java/GitHubRepoInfo/GitHubRepoInfoApplication.java
@@ -0,0 +1,13 @@
+package GitHubRepoInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRepoInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubRepoInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GitHubRepoInfo/GitHubRepoInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package GitHubRepoInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepoInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
